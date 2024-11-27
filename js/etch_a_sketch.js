240178ede1a8773d0be22b57e58fb33f9231f66b
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2024-11-26 18:47:54 -0600

    Refactor grid creation into a function

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -1,5 +1,4 @@
-const GRID_WIDTH = 16;
-const GRID_HEIGHT = 16;
+const GRID_SIZE_BASE = 16;
 
 const gridContainer = document.querySelector(".grid-container");
 
@@ -9,22 +8,30 @@ function changeBackgroundColor(event) {
     event.target.style.backgroundColor = COLOR_TO_APPLY;
 }
 
-// Variables named x and y to represent the position of the grid cards on an
-// xy plane with the top left corner being (0, 0)
-for (let y = 0; y < GRID_HEIGHT; y++) {
-    const gridRow = document.createElement("div");
-    gridRow.classList.add("grid-row");
-    gridRow.style.height = `${100 / GRID_HEIGHT}%`;
-    gridRow.style.display = "flex";
-
-    for (let x = 0; x < GRID_WIDTH; x++) {
-	const gridCard = document.createElement("div");
-	gridCard.classList.add("grid-card");
-	gridCard.style.flex = "auto";
+function createGrid(sideLength = GRID_SIZE_BASE) {
+    // Variables named x and y to represent the position of the grid cards on an
+    // xy plane with the top left corner being (0, 0)
+    for (let y = 0; y < sideLength; y++) {
+	const gridRow = document.createElement("div");
+	gridRow.classList.add("grid-row");
+	gridRow.style.height = `${100 / sideLength}%`;
+	gridRow.style.display = "flex";
+	
+	for (let x = 0; x < sideLength; x++) {
+	    const gridCard = document.createElement("div");
+	    gridCard.classList.add("grid-card");
+	    gridCard.style.flex = "auto";
 	gridRow.appendChild(gridCard);
+	}
+	
+	gridContainer.appendChild(gridRow);
+    }
+}
     }
 
     gridContainer.appendChild(gridRow);
 }
+
+createGrid(GRID_SIZE_BASE);
 
 gridContainer.addEventListener("mouseover", changeBackgroundColor);
